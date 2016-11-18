2a4f4fc5109083a0bea9d7101992c0cc4ed7cf19
Wolfgang Hoenig <whoenig@usc.edu>
2016-11-18 09:58:13 -0800

    remove dependency to NatNetLinux from optitrack.h; code re-formatting

--- include/libmotioncapture/optitrack.h
+++ include/libmotioncapture/optitrack.h
@@ -1,35 +1,36 @@
 #pragma once
 #include "libmotioncapture/motioncapture.h"
 
-//NatNet
-#include <NatNetLinux/NatNet.h>
-
 namespace libmotioncapture {
-    class MotionCaptureOptitrackImpl;
-    
-    class MotionCaptureOptitrack : public MotionCapture{
-    public:
-        MotionCaptureOptitrack(std::string localIp, std::string serverIp);
-        ~MotionCaptureOptitrack();
-        
-        const std::string & version() const;
-        
-        virtual void waitForNextFrame();
-        virtual void getObjects(std::vector<Object>& result) const;
-        //virtual void getObjectByName(const std::string & name, Object & result) const;
-        
-        virtual void getPointCloud(pcl::PointCloud<pcl::PointXYZ>::Ptr result) const;
-        
-        virtual void getLatency(std::vector<LatencyInfo> & result) const;
-        
-        virtual bool supportsObjectTracking() const;
-        virtual bool supportsLatencyEstimate() const;
-        virtual bool supportsPointCloud() const;
-        
-        Point3f axisMultiplier;
-        Point3f axisOrder;
-    private:
-        MotionCaptureOptitrackImpl * pImpl;
-    };
+  class MotionCaptureOptitrackImpl;
+
+  class MotionCaptureOptitrack : public MotionCapture{
+  public:
+    MotionCaptureOptitrack(
+      const std::string& localIp,
+      const std::string& serverIp);
+
+    virtual ~MotionCaptureOptitrack();
+
+    const std::string& version() const;
+
+    virtual void waitForNextFrame();
+    virtual void getObjects(
+      std::vector<Object>& result) const;
+    //virtual void getObjectByName(const std::string & name, Object & result) const;
+
+    virtual void getPointCloud(
+      pcl::PointCloud<pcl::PointXYZ>::Ptr result) const;
+
+    virtual void getLatency(
+      std::vector<LatencyInfo>& result) const;
+
+    virtual bool supportsObjectTracking() const;
+    virtual bool supportsLatencyEstimate() const;
+    virtual bool supportsPointCloud() const;
+
+  private:
+    MotionCaptureOptitrackImpl * pImpl;
+  };
 }
 
