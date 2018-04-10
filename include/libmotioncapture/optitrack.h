b21980da121efc057852c7008abe0914db3838b3
Jinho Kim <netkimjh@gmail.com>
2018-04-10 18:37:26 -0400

    Fixed rigid body fucntion for OptiTrack. Now, <object_tracking_type> can be selected by <motionCapture>.

--- include/libmotioncapture/optitrack.h
+++ include/libmotioncapture/optitrack.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "libmotioncapture/motioncapture.h"
+#include "NatNetLinux/NatNet.h"
 
 namespace libmotioncapture {
   class MotionCaptureOptitrackImpl;
@@ -17,11 +18,12 @@ namespace libmotioncapture {
     virtual void waitForNextFrame();
     virtual void getObjects(
       std::vector<Object>& result) const;
-    //virtual void getObjectByName(const std::string & name, Object & result) const;
+    virtual void getObjectByRigidbody(
+      const RigidBody& rb,
+      Object& result) const;
 
     virtual void getPointCloud(
       pcl::PointCloud<pcl::PointXYZ>::Ptr result) const;
-
     virtual void getLatency(
       std::vector<LatencyInfo>& result) const;
 
